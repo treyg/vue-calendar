e1ba3f3b9977360c397334e8b64b5bb96a0d219c
Trey <trey@pop-os.localdomain>
2021-05-19 16:34:41 -0400

    add signup component

---
+++ src/views/Signup.vue
@@ -0,0 +1,74 @@
+<template>
+  <div>
+    <v-form align="center" @submit.prevent="signupUser">
+      <v-container>
+        <v-row>
+          <v-col cols="12" md="4">
+            <v-text-field
+              v-model="email"
+              label="E-mail"
+              required
+            ></v-text-field>
+            <v-text-field
+              v-model="password"
+              label="Password"
+              required
+              @click:append="() => (value = !value)"
+              :type="value ? 'password' : 'text'"
+              :append-icon="value ? 'mdi-eye' : 'mdi-eye-off'"
+            ></v-text-field>
+
+            <v-btn block @click="signupUser" color="primary"> Sign Up </v-btn>
+          </v-col>
+        </v-row>
+      </v-container>
+      <p class="subtitle-1">
+        Already have an account?
+        <router-link style="text-decoration: none; color: inherit" to="/login"
+          >Login</router-link
+        >
+      </p>
+    </v-form>
+  </div>
+</template>
+<script>
+import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = "https://terbqcgxfxoyxgztbilu.supabase.co";
+const supabaseKey = process.env.VUE_APP_SUPABASE_KEY;
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default {
+  name: "Login",
+  data() {
+    return {
+      email: null,
+      password: null,
+      value: String,
+    };
+  },
+  methods: {
+    signupUser() {
+      supabase.auth
+        .signUp({
+          email: this.email,
+          password: this.password,
+        })
+        .then((response) => {
+          console.log(response);
+          return response;
+        })
+        .catch((error) => {
+          if (error) {
+            console.log(error);
+          }
+        });
+    },
+  },
+};
+</script>
+<style scoped>
+a {
+  text-decoration: underline;
+  border-bottom: 1px solid black;
+}
+</style>
