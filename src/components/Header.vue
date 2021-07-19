3d60aec3bd425eeb4b368afbad29299532f5bedd
Trey <tag@treyg.dev>
2021-07-19 19:14:07 -0400

    Test getting vuex state from other components

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,22 +4,32 @@
     <!-- <router-link to="/"><h4>Reminders</h4></router-link>
     <router-link to="/about"><h4>Calendar</h4></router-link> -->
     <h1>{{ $route.name }}</h1>
+    <h2>Welcome {{ checkMail }}</h2>
   </header>
 </template>
 
 <script>
 export default {
   name: "Header",
-  methods: {
-    showData() {
-      console.log(this.$store.state.userIn);
+  // data() {
+  //   return {
+  //     loggedIn: null,
+  //     userEmail: null,
+  //   };
+  // },
+  computed: {
+    // checkLog() {
+    //   return this.loggedIn == this.$store.state.userInformation.aud;
+    // },
+    checkMail() {
+      return this.$store.state.userInformation.email;
     },
   },
 };
 </script>
 <style scoped>
-h1 {
+/* h1 {
   font-size: 3em;
   font-weight: normal;
-}
+} */
 </style>
